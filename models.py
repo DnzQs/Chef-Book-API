313d9ab7e63e58e7123bd1ef6acda51f94247a5a
Denis Kolesnichenko <dentv2017@gmail.com>
2026-06-03 19:45:50 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Text
+from sqlalchemy.orm import relationship
+
+from app.db import Base
+
+class RecipeIngredient(Base):
+    __tablename__ = "recipe_ingredients"
+
+    recipe_id = Column(Integer, ForeignKey("recipes.id", ondelete="CASCADE"), primary_key=True)
+    ingredient_id = Column(Integer, ForeignKey("ingredients.id", ondelete="CASCADE"), primary_key=True)
+
+    amount = Column(Float, nullable=False)  # Количество (например, 2.5 или 500)
+    unit = Column(String(50), nullable=False)  # Единица измерения (г, мл, шт)
+
+    # Связи для быстрого доступа из связующей таблицы
+    recipe = relationship("Recipe", back_populates="ingredient_associations")
+    ingredient = relationship("Ingredient", back_populates="recipe_associations")
+
+
+class Ingredient(Base):
+    __tablename__ = "ingredients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, index=True, nullable=False)
+
+    recipe_associations = relationship("RecipeIngredient", back_populates="ingredient")
+
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), index=True, nullable=False)
+    description = Column(Text, nullable=True)
+    instructions = Column(Text, nullable=False)
+
+    ingredient_associations = relationship(
+        "RecipeIngredient",
+        back_populates="recipe",
+        cascade="all, delete-orphan"
+    )
