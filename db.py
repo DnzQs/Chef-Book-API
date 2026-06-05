63573b07df4c8f7f12ea5ffc69268e42cca746ef
Denis Kolesnichenko <dentv2017@gmail.com>
2026-06-05 19:24:23 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -1,28 +1,18 @@
-import os
-from dotenv import load_dotenv
-
+from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from .config import settings
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set")
-
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
-)
+    bind=engine)
 
 Base = declarative_base()
 
-
-def get_db():
+def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
