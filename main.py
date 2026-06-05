2c7fc79d3fd5f73d202be6e29a5583c9305a8f50
Denis Kolesnichenko <dentv2017@gmail.com>
2026-06-05 19:31:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI
+from app.core.db import engine, Base
 
-app = FastAPI()
+from app.ingredients.ingredients_router import router as ingredients_router
+from app.recipes.recipes_router import router as recipes_router
+
+from app.ingredients.ingredient_models import Ingredient
+from app.recipes.recipe_models import Recipe, RecipeIngredient
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Chef Book API",
+    description="API для поиска рецептов по имеющимся ингредиентам",
+    version="1.0.0"
+)
+
+app.include_router(ingredients_router, prefix="/api/v1")
+app.include_router(recipes_router, prefix="/api/v1")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome     to Chef Book API! Go to /docs for Swagger."}
